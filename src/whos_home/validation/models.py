85815f5df5ef958a07e6306a53a16cc6cb28f69e
f1nesser <riley.iwanaga@colorado.edu>
2022-11-12 01:03:03 -0700

    django app and models

---
+++ src/whos_home/validation/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class DoorEvent(models.Model):
+    date_time = models.DateTimeField('datetime door opened')
+    day_of_the_week = models.IntegerField(default=-1)
+    whos_there = models.ForeignKey(Person, on_delete=models.CASCADE)
+    ACTION_CHOICES = [('EN', 'entering'), ('LE', 'leaving')]
+    action = models.CharField(max_length=2, choices=ACTION_CHOICES)
+
+    def __str__(self):
+        return f'{self.date_time}, {self.whos_there}, {self.action}'
